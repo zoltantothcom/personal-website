d842ad1037fb582d71278a97bc37430fa27c18a9
Zoltan Toth <guest233@canada01.ifdsnet.int>
2017-03-15 10:18:08 -0400

    button listeners for console

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,1 +1,82 @@
-console.log('hello, world!');
+window.addEventListener('load', function() {
+	document.querySelector('.console').classList.add('visible');
+});
+
+var messages = {
+	help: 'List of available commands:',
+	default: 'is not recognized as an internal or external command.'
+};
+
+var Console = function() {
+	var input   = document.querySelector('.console input'),
+		command = '',
+		self = this;
+
+	self.addListener('.console__button--cls', 'click', self.close);
+	self.addListener('.console__button--min', 'click', self.hide);
+	self.addListener('.console__button--max', 'click', self.show);
+
+	input.addEventListener('keyup', function(e) {
+		if (e.keyCode === 13) {
+			command = input.value;
+
+			switch (command) {
+				case '--help':
+				case   'help':
+					console.log(messages.help);
+					break;
+
+				case '--clean':
+				case   'clean':
+					self.clean();
+					break;
+
+				case '--hide':
+				case   'hide':
+					self.hide();
+					break;
+
+				case '--show':
+				case   'show':
+					self.show();
+					break;
+
+				case '--close':
+				case   'close':
+					self.close();
+					break;
+
+				default:
+					console.log('\'' + command + '\' ' + messages.default);
+					console.log(messages.help);
+			}
+
+			input.value = '';
+		}
+	});
+
+};
+
+Console.prototype.addListener = function(elem, event, action) {
+	document.querySelector(elem).addEventListener(event, function() {
+		action();
+	});
+};
+
+Console.prototype.clean = function() {
+	document.querySelector('.console__message-area').innerHTML = '';
+};
+
+Console.prototype.hide = function() {
+	document.querySelector('.console__window').style.height = '0';
+};
+
+Console.prototype.show = function() {
+	document.querySelector('.console__window').style.height = '370px';
+};
+
+Console.prototype.close = function() {
+	document.querySelector('.console').style.display = 'none';
+};
+
+var cmd = new Console();
