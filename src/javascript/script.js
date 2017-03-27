4f63451a08fec3ed269a1abcecdc9149e0a3d66e
Zoltan Toth <ztoth@ifdsgroup.com>
2017-03-27 13:37:31 -0400

    commands history

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -4,19 +4,22 @@ window.addEventListener('load', function() {
 });
 
 var messages = {
+	default: [
+		'type <span class="console__success">-h</span> or <span class="console__success">help</span> and press Enter for available commands'
+	],
 	error: [
 		' is not recognized as a valid command.'
 	],
 
 	help: [
-		'<span class="console__warning">-a, about</span> ... A really short bio',
-		'<span class="console__warning">-s, social</span> .. Links to LinkedIn, StackOverflow, GitHub',
-		'<span class="console__warning">-c, contact</span> . Email and phone for quick contact',
-		'<span class="console__warning">-h, help</span> .... Available commands',
+		'<span class="console__success">-a</span> or <span class="console__success">about</span> ... A really short bio',
+		'<span class="console__success">-s, social</span> .. Links to LinkedIn, StackOverflow, GitHub',
+		'<span class="console__success">-c, contact</span> . Email and phone for quick contact',
+		'<span class="console__success">-h, help</span> .... Available commands',
 		'',
-		'<span class="console__warning">-x, exit</span> .... Close this window',
-		'<span class="console__warning">-m, hide</span> .... Minimize this window',
-		'<span class="console__warning">-r, clean</span> ... Clean this window',
+		'<span class="console__success">-x, exit</span> .... Close this window',
+		'<span class="console__success">-m, hide</span> .... Minimize this window',
+		'<span class="console__success">-d, clean</span> ... Clean this window',
 	],
 
 	social: [
@@ -40,16 +43,25 @@ var Console = function() {
 		command = '',
 		self = this;
 
+	self.history = [];
+
 	self.addListener('.console__button--cls', 'click', self.close);
 	self.addListener('.console__button--min', 'click', self.hide);
 	self.addListener('.console__button--max', 'click', self.show);
 
+	// show default message to help start using the console
+	self.output('default');
+
 	input.addEventListener('keyup', function(e) {
-		if (e.keyCode === 13) {
+		// 38 - arrow up
+		// 40 - arrow down
+		if (e.keyCode === 38) {
+			input.value = self.history[0] || '';
+		} else if (e.keyCode === 13) {
 			command = input.value;
 
 			switch (command) {
-				case '-r':
+				case '-d':
 				case 'clean':
 					self.clean();
 					break;
@@ -59,7 +71,7 @@ var Console = function() {
 					self.hide();
 					break;
 
-				case '-d':
+				case '-p':
 				case 'show':
 					self.show();
 					break;
@@ -86,10 +98,15 @@ Console.prototype.addListener = function(elem, event, action) {
 };
 
 Console.prototype.output = function(command) {
-	var div = document.createElement('p'),
-		txt;
+	var div = document.createElement('p');
 
-	div.innerHTML = '> ' + command + '<br>';
+	if (command !== 'default') {
+		// push the command to the history array
+		this.history.unshift(command);
+
+		// show the command
+		div.innerHTML = '> ' + command + '<br>';
+	}
 
 	switch (command) {
 		case '-a':
@@ -115,17 +132,20 @@ Console.prototype.output = function(command) {
 			div.innerHTML += messages[command][i] + '<br>';
 		}
 	} else {
-		div.innerHTML += '<span class="console__error">ERROR! '+ "'"  + command + "'" + messages.error + '</span>';
+		div.innerHTML += '<span class="console__error">ERROR! '+ '\''  + command + '\'' + messages.error + '</span>';
 	}
 
 	document.querySelector('.console__message-area').appendChild(div);
 
-	var container = document.querySelector(".console__window");
+	var container = document.querySelector('.console__window');
+
+	// always scroll to bottom
 	container.scrollTop = container.scrollHeight;
 };
 
 Console.prototype.clean = function() {
 	document.querySelector('.console__message-area').innerHTML = '';
+	this.output('default');
 };
 
 Console.prototype.hide = function() {
