361236feffd4fe938c248d78689b42eb35bdee75
Zoltan Toth <ztoth@indigo.ca>
2017-11-15 17:32:07 -0500

    ryerson logo added

--- src/javascript/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-
-};
-
-// portfolio
-document.querySelector('.Portfolio').addEventListener('click', function(e) {
-	if (e.target.nodeName !== 'I') return;
-	e.target.parentNode.parentNode.classList.toggle('is-flip');
-});
