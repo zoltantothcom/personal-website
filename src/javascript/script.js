fd2103b29ffc20fdaea476650b3d10d104955b14
Zoltan Toth <ztoth@indigo.ca>
2017-11-23 16:11:48 -0500

    contact form styling improved

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -9,9 +9,27 @@ document.querySelector('.Portfolio').addEventListener('click', function(e) {
 });
 
 
-document.getElementById('name').addEventListener('focus', function(e) {
-	e.target.parentNode.classList.add('is-active', 'is-completed');
-});
-document.getElementById('name').addEventListener('blur', function(e) {
-	e.target.parentNode.classList.remove('is-active');
-});
+var inputs = document.querySelectorAll('.Contact-input');
+console.log(inputs)
+
+for (var i = 0; i < inputs.length; i++) {
+	inputs[i].addEventListener('focus', function(e) {
+		e.target.parentNode.classList.add('is-active', 'is-completed');
+	});
+
+	inputs[i].addEventListener('blur', function(e) {
+		e.target.parentNode.classList.remove('is-active');
+
+		if (e.target.value === '') {
+			e.target.parentNode.classList.remove('is-completed');
+		}
+	});
+}
+
+
+// document.getElementById('name').addEventListener('focus', function(e) {
+// 	e.target.parentNode.classList.add('is-active', 'is-completed');
+// });
+// document.getElementById('name').addEventListener('blur', function(e) {
+// 	e.target.parentNode.classList.remove('is-active');
+// });
