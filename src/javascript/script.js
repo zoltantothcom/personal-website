0263af6d9bbdadbae3c9de72b3daf9b4defed113
Zoltan Toth <ztoth@indigo.ca>
2017-11-22 20:42:14 -0500

    input effect POC created

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -7,3 +7,11 @@ document.querySelector('.Portfolio').addEventListener('click', function(e) {
 	if (e.target.nodeName !== 'I') return;
 	e.target.parentNode.parentNode.classList.toggle('is-flip');
 });
+
+
+document.getElementById('name').addEventListener('focus', function(e) {
+	e.target.parentNode.classList.add('is-active', 'is-completed');
+});
+document.getElementById('name').addEventListener('blur', function(e) {
+	e.target.parentNode.classList.remove('is-active');
+});
