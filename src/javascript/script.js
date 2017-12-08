8bcf760624f43eb623e3ada4b3b6ecc5bd90e60d
Zoltan Toth <ztoth@indigo.ca>
2017-12-08 13:43:46 -0500

    contact form submit temporarirly taken care of with a warning message

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,54 +1,58 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function (e) {
+	// menu
+	document.querySelector('.menu').addEventListener('click', function (e) {
+		e.preventDefault();
 
-};
+		if (e.target.className !== 'menu__link') return;
 
-// menu
-document.querySelector('.menu').addEventListener('click', function(e) {
-	e.preventDefault();
+		var parts = e.target.href.split('/');
 
-	if (e.target.className !== 'menu__link') return;
-
-	var parts = e.target.href.split('/');
-
-	document.querySelector(parts[parts.length - 1]).scrollIntoView({ 
-		behavior: 'smooth',
-		block: 'start'
-	});
-});
-
-// portfolio
-document.querySelector('.portfolio').addEventListener('click', function(e) {
-	if (e.target.classList.contains('portfolio__item-front')) {
-		e.target.parentNode.parentNode.classList.add('hover');
-	}
-
-	if (e.target.classList.contains('hover')) {
-		e.target.classList.remove('hover');
-	}
-});
-
-// contact
-var inputs = document.querySelectorAll('.contact__input');
-
-for (var i = 0; i < inputs.length; i++) {
-	inputs[i].addEventListener('focus', function(e) {
-		e.target.parentNode.classList.add('is-active', 'is-completed');
+		document.querySelector(parts[parts.length - 1]).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		});
 	});
 
-	inputs[i].addEventListener('blur', function(e) {
-		e.target.parentNode.classList.remove('is-active');
+	// portfolio
+	document.querySelector('.portfolio').addEventListener('click', function (e) {
+		if (e.target.classList.contains('portfolio__item-front')) {
+			e.target.parentNode.parentNode.classList.add('hover');
+		}
 
-		if (e.target.value === '') {
-			e.target.parentNode.classList.remove('is-completed');
+		if (e.target.classList.contains('hover')) {
+			e.target.classList.remove('hover');
 		}
 	});
-}
 
+	// contact
+	var inputs = document.querySelectorAll('.contact__input');
 
-var textarea = document.querySelector('.is-textarea');
-var offset = textarea.offsetHeight - textarea.clientHeight;
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].addEventListener('focus', function (e) {
+			e.target.parentNode.classList.add('is-active', 'is-completed');
+		});
 
-textarea.addEventListener('input', function() {
-    this.style.height = 'auto';
-    this.style.height = this.scrollHeight + offset + 'px';
-}, false);
+		inputs[i].addEventListener('blur', function (e) {
+			e.target.parentNode.classList.remove('is-active');
+
+			if (e.target.value === '') {
+				e.target.parentNode.classList.remove('is-completed');
+			}
+		});
+	}
+
+	var textarea = document.querySelector('.is-textarea');
+	var offset = textarea.offsetHeight - textarea.clientHeight;
+
+	textarea.addEventListener('input', function () {
+		this.style.height = 'auto';
+		this.style.height = this.scrollHeight + offset + 'px';
+	}, false);
+
+	// temp
+	document.querySelector('.contact__button').addEventListener('click', function (e) {
+		e.preventDefault();
+
+		document.querySelector('.contact__under-development').classList.remove('is-hidden');
+	});
+});
